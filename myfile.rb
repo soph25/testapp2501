1cdcd4fba206192f1809f64b1fdc40423eea0156
sophie <iguane25@laposte.net>
2019-02-01 17:22:35 +0100

    Create myfile.rb

---
+++ myfile.rb
@@ -0,0 +1,16 @@
+class Octocat
+  def initialize(name, *breeds)
+    # Instance variables
+    @name = name
+    @breeds = breeds
+  end
+
+  def display
+    breed = @breeds.join("-")
+
+    puts "I am of #{breed} breed, and my name is #{@name}."
+  end
+end
+
+m = Octocat.new("Mona", "cat", "octopus")
+m.display
